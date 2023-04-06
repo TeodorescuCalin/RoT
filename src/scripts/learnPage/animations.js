bb851eed2f169f05be38b9175dc55f847ff73f5c
StefanPaulet <tudor.stefan.paulet@gmail.com>
2023-04-06 09:15:42 +0300

    Began the style for second type of questions

--- src/scripts/learnPage/animations.js
+++ src/scripts/learnPage/animations.js
@@ -1,10 +1,10 @@
-document.querySelectorAll('.questionContainer__answerBlock__answer')
+document.querySelectorAll('.questionContainer__answerBlock__choiceAnswer')
     .forEach(element => {
         element.addEventListener('click', () => {
-            if ( element.classList.contains('questionContainer__answerBlock__answer--selected')) {
-                element.classList.remove('questionContainer__answerBlock__answer--selected');
+            if ( element.classList.contains('questionContainer__answerBlock__choiceAnswer--selected')) {
+                element.classList.remove('questionContainer__answerBlock__choiceAnswer--selected');
             } else {
-                element.classList.add('questionContainer__answerBlock__answer--selected');
+                element.classList.add('questionContainer__answerBlock__choiceAnswer--selected');
             }
         })
     });
