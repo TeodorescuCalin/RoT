e78d03b56b8d66cf975ac8cb84c760b72d23dbc4
StefanPaulet <tudor.stefan.paulet@gmail.com>
2023-04-07 21:55:49 +0300

    Safety commit before merging

--- src/scripts/navBar/navbarPushOnExpand.js
+++ src/scripts/navBar/navbarPushOnExpand.js
@@ -4,6 +4,6 @@ expandCheckBox.addEventListener('change', element => {
     if ( element.target.checked === true ) {
         document.getElementById("navigationBar").style.marginBottom = "700px";
     } else {
-        document.getElementById("navigationBar").style.marginBottom = "0px";
+        document.getElementById("navigationBar").style.marginBottom = "200px";
     }
 })
